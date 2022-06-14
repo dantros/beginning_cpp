5ee9e8ae0f6d8ecb91417420b76ab127f44234a4
Daniel Calderon <21376367+dantros@users.noreply.github.com>
2022-06-13 23:04:18 -0400

    basic mutex example

--- concurrency/02_hello_mutex/source/hello_mutex.cpp
+++ concurrency/02_hello_mutex/source/hello_mutex.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <thread>
+#include <mutex>
 #include <iomanip>
 #include <iso646.h>
 #include <cstdint>
@@ -25,7 +26,7 @@ bool isPrime(const std::uint64_t number)
     return true;
 }
 
-void searchForPrimeNumbers(const std::uint64_t first, const std::uint64_t last, std::vector<std::uint64_t>& primeNumbers, const char monitor)
+void searchForPrimeNumbers(std::mutex& storageMutex, const std::uint64_t first, const std::uint64_t last, std::vector<std::uint64_t>& primeNumbers, const char monitor)
 {
     static constexpr std::uint64_t IterationsToMonitor = static_cast<std::uint64_t>(1e6);
 
@@ -33,7 +34,11 @@ void searchForPrimeNumbers(const std::uint64_t first, const std::uint64_t last, 
     {
         if (isPrime(number))
         {
+            storageMutex.lock();
+            // critical section
             primeNumbers.push_back(number);
+            // end of critical section
+            storageMutex.unlock();
         }
         if ((number - first) % IterationsToMonitor == 0)
         {
@@ -47,14 +52,16 @@ void searchForPrimeNumbers(const std::uint64_t first, const std::uint64_t last, 
 
 void main() 
 {
-    std::vector<std::uint64_t> primeNumbers1, primeNumbers2, primeNumbers3, primeNumbers4;
+    std::mutex storageMutex;
+
+    std::vector<std::uint64_t> primeNumbers;
 
     // spawn and start threads
     // each thread has its own local storage. To pass by reference, we need std::ref.
-    std::thread thread1(searchForPrimeNumbers, static_cast<std::uint64_t>(0),   static_cast<std::uint64_t>(1e7), std::ref(primeNumbers1), '1');
-    std::thread thread2(searchForPrimeNumbers, static_cast<std::uint64_t>(1e7), static_cast<std::uint64_t>(5e7), std::ref(primeNumbers2), '2');
-    std::thread thread3(searchForPrimeNumbers, static_cast<std::uint64_t>(5e7), static_cast<std::uint64_t>(1e8), std::ref(primeNumbers3), '3');
-    std::thread thread4(searchForPrimeNumbers, static_cast<std::uint64_t>(1e8), static_cast<std::uint64_t>(2e8), std::ref(primeNumbers4), '4');
+    std::thread thread1(searchForPrimeNumbers, std::ref(storageMutex), static_cast<std::uint64_t>(0),   static_cast<std::uint64_t>(1e7), std::ref(primeNumbers), '1');
+    std::thread thread2(searchForPrimeNumbers, std::ref(storageMutex), static_cast<std::uint64_t>(1e7), static_cast<std::uint64_t>(5e7), std::ref(primeNumbers), '2');
+    std::thread thread3(searchForPrimeNumbers, std::ref(storageMutex), static_cast<std::uint64_t>(5e7), static_cast<std::uint64_t>(1e8), std::ref(primeNumbers), '3');
+    std::thread thread4(searchForPrimeNumbers, std::ref(storageMutex), static_cast<std::uint64_t>(1e8), static_cast<std::uint64_t>(2e8), std::ref(primeNumbers), '4');
 
     // wating for both threads to finish
     thread1.join();
@@ -63,11 +70,7 @@ void main()
     thread4.join();
 
     std::cout << "We found: "
-        << primeNumbers1.size() << " + " 
-        << primeNumbers2.size() << " + " 
-        << primeNumbers3.size() << " + " 
-        << primeNumbers4.size() << " = "
-        << primeNumbers1.size() + primeNumbers2.size() + primeNumbers3.size() + primeNumbers4.size()
+        << primeNumbers.size()
         << " prime numbers." << std::endl;
     std::cout << "We are done for today." << std::endl;
 }
