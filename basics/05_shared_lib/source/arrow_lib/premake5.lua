835403e3b4d23f0c577be3dacd491d7f1f54dc49
Daniel Calderon <21376367+dantros@users.noreply.github.com>
2024-07-22 21:21:15 -0400

    Adding premake example for a shared library

---
+++ basics/05_shared_lib/source/arrow_lib/premake5.lua
@@ -0,0 +1,42 @@
+
+project "arrow_library"
+    location "%{wks.location}/arrow_library"
+    -- Building as a shared library
+    kind "SharedLib"
+    language "C++"
+    cppdialect "C++20"
+
+    -- with this macro, we add the __declspec(dllexport) / __declspec(dllexport) to the classes and
+    -- functions of the public API of this shared library
+    defines { "ARROW_LIB" }
+
+    -- We are locating the lib in the same directory than the executable.
+    -- but it could be anywhere else...
+    targetdir "%{wks.location}/%{cfg.buildcfg}"
+
+    -- Considering only the files related to this library
+    -- for ilustration purposes, we are using wildcards
+    files { "source/*.cpp" }
+
+    -- ... but we could also have listed each file independently
+    --[[
+    files {
+        "sources/arrow_functions.cpp",
+        "sources/arrow.cpp",
+        "sources/point_functions.cpp",
+        "sources/triangle.cpp"
+    }
+    ]]--
+
+    -- since headers are not located next to the source files, we need to specify the path where to look for headers
+    -- this way, we can include header files in the cpp files simply as '#include "myheader.h"'
+    includedirs {"include"}
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+
